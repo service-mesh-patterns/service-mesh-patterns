de9593ca1211586c6aa3394c193279e865b77746
Nikhil Ladha <nikhilladha1999@gmail.com>
2022-09-25 20:30:54 +0530

    Add Navbar component to chapters page
    Signed-off-by: Nikhil Ladha <nikhilladha1999@gmail.com>

--- site/src/pages/chapters.js
+++ site/src/pages/chapters.js
@@ -2,7 +2,8 @@ import React from "react";
 import ChaptersPage from "../sections/Chapters";
 import Layout from "../components/layout";
 import SeoComponent from "../components/seo";
-import Footer from "../components/Footer/index";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Chapters = () => {
   return (
@@ -12,6 +13,7 @@ const Chapters = () => {
         description="Chapters and topics found in the Service Mesh Patterns book"
       />
       <Layout>
+        <Navbar />
         <ChaptersPage />
         <Footer />
       </Layout>
